d1e4e931217d282456f78af587c5f15e14c1289d
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2023-02-09 15:31:43 -0500

    Fixed issues with <0.01 being a possible value for utilization on in the utilization report

--- scripts/lib/utilization_image_gen.py
+++ scripts/lib/utilization_image_gen.py
@@ -31,7 +31,7 @@ def gen_images(root_copy_dir, fi, verbose, dry_run, selected_properties, headers
         imgkit.from_string(p.get_html_string(attributes={"style":"margin-left:auto;margin-right:auto;width:500px"}), "{}table.jpg".format(image_file_path), options={'crop-x':'250', 'crop-w':'525', 'log-level':'none'})
 
         df = pd.DataFrame.from_dict(data, orient='index', columns=headers[1:])
-        labels = [float(f) for f in df[headers[3]].values]
+        labels = [float(''.join(c for c in f if (c.isdigit() or c =='.'))) for f in df[headers[3]].values]
         fig, ax = plt.subplots()
         ax.bar(df.index.values, labels)
         ax.set_title('Resource Utilization')
