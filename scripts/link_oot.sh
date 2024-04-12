1093e4f546cab1af4e39f000aaffd1fab57efd58
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2024-04-12 18:25:04 -0400

    Updating linking scripts to ignore the utilization directory for OOT modules

--- scripts/link_oot.sh
+++ scripts/link_oot.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 subdircount=$(find "$PWD/oot" -maxdepth 1 -type d | wc -l)
+files=$(find "$PWD/oot" -maxdepth 2 -mindepth 2 -type d | grep -Ev utilization | grep -Ev ".git")
 
 if [[ "$subdircount" -eq 1 ]]
 then
     echo "No OOT directories found"
 else
-    cp -anl "$PWD/oot/"**/* ./
+    for f in $files; do
+        cp -anl "$f" ./
+    done
 fi
--- scripts/unlink_oot.sh
+++ scripts/unlink_oot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-files=$(find oot/ -name .git -type d -prune -o -type f -print)
-other_files=$(find . -xdev \( -path ./oot -o -name build -type d -o -name .git -type d -o -name *pycache* -type d \) -prune -o -type f -print)
+files=$(find oot/ -name .git -type d -prune -o -name utilization -type d -prune -o -type f -print)
+other_files=$(find . -xdev \( -path ./oot -o -name build -type d -o -name .git -type d -o -name *pycache* -type d -o -name utilization -type d \) -prune -o -type f -print)
 
 for f in $files; do
     for nf in $other_files; do
