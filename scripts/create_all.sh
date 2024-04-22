3a35f33cc830ea8d6e7f45db8dc266d96fad72b9
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2024-04-22 14:59:25 -0400

    Adding a script to make it easier to build all the parts needed for Yocto builds as a batch

---
+++ scripts/create_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: create_all.sh [build_directory]"
+    exit 1
+fi
+
+for f in $(find $1 -name *.xsa | grep "/default/")
+do
+    xsa_file=$f
+    xsa_path=$(dirname -- "$xsa_file")
+    echo "Operating on $xsa_file"
+    ./scripts/create_dts.sh   "$xsa_file" "$xsa_path"/dts.zip
+    ./scripts/create_fsbl.sh  "$xsa_file" "$xsa_path"/fsbl.zip
+    ./scripts/create_pmufw.sh "$xsa_file" "$xsa_path"/pmufw.zip
+done
