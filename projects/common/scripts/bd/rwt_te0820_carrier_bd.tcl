4efd425087a5f8567f17fbd43d543806b6754d1d
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2024-07-17 15:22:31 -0400

    Updating to fix changes in the defaults for the way the SD card is configured

--- projects/common/scripts/bd/rwt_te0820_carrier_bd.tcl
+++ projects/common/scripts/bd/rwt_te0820_carrier_bd.tcl
@@ -107,6 +107,8 @@ set_property -dict [list \
 set_property -dict [list \
   CONFIG.PSU__SD0__PERIPHERAL__ENABLE {1} \
   CONFIG.PSU__SD0__SLOT_TYPE {eMMC} \
+  CONFIG.PSU__SD0__DATA_TRANSFER_MODE {8Bit} \
+  CONFIG.PSU__SD0__PERIPHERAL__IO {MIO 13 .. 22} \
   CONFIG.PSU__SD0__RESET__ENABLE {1} \
 ] [get_bd_cells sys_ps8]
 
