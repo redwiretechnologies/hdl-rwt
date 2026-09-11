ede2f41439c9002eb59fd790b22cda1b8636165b
Jonathan Ambrose <jambrose@redwiretechnologies.us>
2026-09-11 12:09:09 -0400

    Fixes to meet timing

---
+++ projects/common/oxygen/rev3/vivado_directives.tcl
@@ -0,0 +1,6 @@
+set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreArea [get_runs impl_1]
+set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE  AltSpreadLogic_medium [get_runs impl_1]
+set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
+set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
+set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
--- projects/gr-iio/oxygen/rev3/vivado_directives.tcl
+++ projects/gr-iio/oxygen/rev3/vivado_directives.tcl
@@ -3,3 +3,11 @@ set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE AltSpreadLogic_low [get_runs impl
 set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
 set_property STEPS.ROUTE_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
 set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
+
+set full_board [get_parts -of_objects [get_projects]]
+set board [string range $full_board 4 4]
+
+if { ( $board == "4" ) } {
+    set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE ExploreArea [get_runs impl_1]
+    set_property STEPS.PLACE_DESIGN.ARGS.DIRECTIVE ExtraTimingOpt [get_runs impl_1]
+}
